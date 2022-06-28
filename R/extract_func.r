cbcb5598df29d3c927d6e1ed494d565f68c6d3d6
weinbergerlab <dweinber@gmail.com>
2022-06-28 16:14:37 -0400

    calculates standardized ratesand RRs WITH confidence intervals

--- R/extract_func.r
+++ R/extract_func.r
@@ -6,9 +6,9 @@ extract_func <- function(ds, measure=c('RR','excess','excess_inc'), round.level=
   
   formatted.res <- paste0(med[1], ' (95%CI:', med[2],', ', med[3],')'  )
   
-  ds$preds.age <- ds$preds.age[ds$preds.age$date>='2020-01-01', c('agec','date',measures)]
+ # ds$preds.age <- ds$preds.age[ds$preds.age$date>='2020-01-01', c('agec','date',measures)]
   
  # formatted.age.qtr <- paste0(round(ds$preds.age[3], round.level), ' (95%CI:', round(ds$preds.age[4],round.level),', ', round(ds$preds.age[5], round.level),')'  )
   
-  return(list('overall.results'=formatted.res, 'age.qtr.results'=ds$preds.age))
+  return(list('overall.results'=formatted.res))
 }
