834e811b91e28674fba35b642502870ed176a896
weinbergerlab <dweinber@gmail.com>
2022-06-29 16:41:38 -0400

    add sex summary table to output

--- R/call_inla.R
+++ R/call_inla.R
@@ -49,6 +49,13 @@ call_inla <- function(label=c('cdc','va'), ds){
     ungroup() %>%
     group_by(source,agec, date) %>%
     summarize_grps_quantiles
+  
+  preds.sex <- res1 %>%
+    group_by(source,sex, date, variable) %>%
+    summarize_grps_sums %>%
+    ungroup() %>%
+    group_by(source,sex, date) %>%
+    summarize_grps_quantiles
    # select(source, agec ,date, starts_with('RR') , starts_with('excess'), starts_with('pred'), starts_with('log_RR'))
   
   preds.age_race <- res1 %>%
@@ -87,6 +94,9 @@ call_inla <- function(label=c('cdc','va'), ds){
   preds.age.out <- preds.age %>%
     select(source, agec, date, pop, starts_with('RR'),starts_with('excess_inc'),starts_with('excess_std_inc'), starts_with('log_RR'),starts_with('obs_inc'),starts_with('obs_std_inc') )
   
+  preds.sex.out <- preds.sex %>%
+    select(source, sex, date, pop, starts_with('RR'),starts_with('excess_inc'),starts_with('excess_std_inc'), starts_with('log_RR'),starts_with('obs_inc'),starts_with('obs_std_inc') )
+  
   preds.age.race.region.out <- preds.age.race.region %>%
     select(source, agec, region, race_recode, sex, date,pop, starts_with('RR'),starts_with('excess_inc'),starts_with('excess_std_inc'), starts_with('log_RR'),starts_with('obs_inc'),starts_with('obs_std_inc')  )
   
@@ -101,7 +111,7 @@ call_inla <- function(label=c('cdc','va'), ds){
   saveRDS(fit.plots, paste0('./outputs/fit.plots.', label, '.rds'))
   saveRDS(rr.plot1, paste0('./outputs/rr.plots.', label, '.rds')) 
   
-  save.list <- list('fit.plots'=fit.plots, 'rr.plot1'=rr.plot1,'preds.age'=preds.age.out,'preds.age.race.region'=preds.age.race.region.out,'preds.age_race'=preds.age_race.out, 'preds.date'= preds.date,'preds.overall'=preds.overall,'preds.age.overall'=preds.age.overall,'preds.age.sex.overall'=preds.age.sex.overall)
+  save.list <- list('fit.plots'=fit.plots, 'rr.plot1'=rr.plot1,'preds.age'=preds.age.out,'preds.sex'=preds.sex.out,'preds.age.race.region'=preds.age.race.region.out,'preds.age_race'=preds.age_race.out, 'preds.date'= preds.date,'preds.overall'=preds.overall,'preds.age.overall'=preds.age.overall,'preds.age.sex.overall'=preds.age.sex.overall)
   saveRDS(save.list,paste0('./outputs/', 'summary_list_',label,'.rds'))
   
  
